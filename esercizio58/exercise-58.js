9fcdb704ebfab34335a39e1a2faace3583358c34
Marco Caputo <93984796+axilo2@users.noreply.github.com>
2022-02-19 20:55:40 +0100

    esercizio-58.js

---
+++ esercizio58/exercise-58.js
@@ -0,0 +1,38 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => { 
+      const personId = persons.find(item => item.id === id)
+      if(personId) {
+        resolve(personId);
+      }else{
+        reject(new Error('Err: Id not found'))
+      }
+    }, 1000);
+  });
+}
+
+fetchPersonById(2).then((person) => console.log(person))
+.catch((err) => { console.log(err)});
+fetchPersonById(4).then((person) => console.log(person))
+.catch((err) => { console.log(err)});
